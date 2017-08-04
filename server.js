9991c5d1f029ba4b25d0560a41b758941e84a0f3
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-08-04 21:27:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function(req,res){
-   res.send('Article three requested and send from here');
+   rres.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
     
 });
 
