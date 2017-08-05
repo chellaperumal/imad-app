10f3fd18f7fdede2f631653e87bcb8fe0d766daf
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-08-05 23:17:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value to be printed';
 
 var img = document.getElementById('madi');
-img.onClick = function(){
+img.onclick = function(){
   img.style.marginLeft = '100px';  
 };
