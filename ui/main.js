c6ec10f626bcc4dafd17cfc8d79f75d32efee9b9
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-08-06 15:36:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,9 +19,8 @@ var button = document.getElementById("counter");
 //var counter = 0;
 button.onclick = function(){
     
-var nameInput = document.getElementById('name');
-    var name = nameInput.value; 
-    var submit = document.getElementById('submit_btn');
+
+var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
             
     
@@ -50,6 +49,8 @@ submit.onclick = function(){
     //var span = document.getElementById("count");
     //span.innerHTML = counter.toString();
     
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value; 
     request.open('GET','http://pondychellam.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };
