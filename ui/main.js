f113f1170951269f74d02f5bac0f88e45b245df6
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-08-07 18:01:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,7 @@ button.onclick = function(){
  };
  
  // Submit Name
- var nameInput = document.getElementById('name');
- var name = nameInput.value;
+ 
  var submit = document.getElementById('submit_btn');
  submit.onclick = function(){
      //Make Request
@@ -52,7 +51,8 @@ button.onclick = function(){
     };
      
      //Make the request
-
+ var nameInput = document.getElementById('name');
+ var name = nameInput.value;
  request.open('GET','http://pondychellam.imad.hasura-app.io/submit-name?name'+ name,true);
  request.send(null);
  };
