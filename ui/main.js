97d0f48929e753b5f78ca923fe7f21c59f2095fd
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-08-07 14:58:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,8 @@
 //Counter Code
 var button = document.getElementById('counter');
-
+var counter = 0;
 button.onclick = function(){
-  
-/*  var request = new XMLHTMLRequest();
-  request.onreadystatechange = function(){
-      if(request.readyState === XMLHTMLRequest.DONE){
-          if(request.status === 200){
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-                span.innerHTML = counter.toString(); 
-  
-            }
-        }
-    };*/
+ 
     var counter = counter + 1;
               var span = document.getElementById('count');
                 span.innerHTML = counter.toString(); 
