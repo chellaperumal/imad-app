3ea026cc8c5a2d02d1f4b849de2001ff646bde18
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-08-13 12:52:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ function crateTemplate (data){
 		            ${heading}
 		        </h3>
 		        <div>
-		            ${date}
+		            ${date.toDateString()}
 		        </div>
 		        <div>
 		        ${content}
