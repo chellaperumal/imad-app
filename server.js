16dfdcadf7bac647f307b2da11fac36612211cf1
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-08-15 12:49:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.post('/login',function(req,res){
            var dbString = result.rows[0].password;       
            var salt = dbString.split('$')[2];
            var hashedPassword = hash(password,salt);
-           if(hashedPassord === dbString){
+           if(hashedPassword === dbString){
               res.send('User Credentials are Correct');
            }
             else {
