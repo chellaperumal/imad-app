76c27078ffd2819ca0ac495249a6ec71ed46cb3d
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-08-17 17:17:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ function crateTemplate (data){
 		        </div>
 		        <div>
 		        <h6>Please add your feedback in the below box</h6>
-		        <input type="text" height="200px" id= ${comment} >
+		        <input type="text" height="200px" width=50% id= ${comment} >
 		        </div>
 		        
 		    </div>
