b3ff98d2e8b80bedae099c5ffcb22a5d38d699de
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-08-23 00:29:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -201,7 +201,7 @@ var submit = document.getElementById('sub_cmt');
      //Make the request
  var cmtInput = document.getElementById('cmnt');
  var cmnt = cmtInput.value;
- request.open('GET','http://pondychellam.imad.hasura-app.io/articles/:articleName?cmntcmnt='+ cmnt,true);
+ request.open('GET','http://pondychellam.imad.hasura-app.io/articles/:articleName?cmnt='+ cmnt,true);
  request.send(null);
  };
 
