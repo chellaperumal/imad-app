2e266a4cb7c29c8c1f5b3cab0b724214bc770b1d
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-09-24 11:03:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -77,4 +77,59 @@ register.onclick = function (){
 };
 }
 
+function loadLoggedInUser (username){
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+    <h3> Hi <i>${username}<i></h3>
+    <a href="/logout">Logout</a>
+    `;
+    
+}
 
+function loadLogin (){
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+    if(request.readystate === XMLHttpRequest.DONE){
+        if(request.status === 200){
+        loadLoggedInUser(this.responseText);
+        }
+        else{
+            loadLoginForm();
+        }
+        
+    }
+    };
+    request.open('GET', '/check-login', true);
+    request.send();
+}
+
+function loadArticles () {
+     // Check if the user is already logged in
+      var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+    if(request.readystate === XMLHttpRequest.DONE){
+        var articles = document.getElementById('articles');
+        if(request.status === 200){
+            var content = '<ul>';
+            var articleData = JSON.parse(this.responeText);
+            for (var i=0; i< articleData.length; i++) {
+                content += `<li>
+                <a href = "/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                (${articleData[i].data.split('T')[0]})</li>`;
+            }
+     content += "</ul>"
+     articles.innerHTML = content;
+        } else {
+            articles.innerHTML('Oops! Could not load all articles!')
+        }
+    }
+    };
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+     
+}
+
+
+loadLogin();
+loadArticles();
