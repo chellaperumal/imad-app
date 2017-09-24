6e5676f8721cfea559a4cf383a1a7228ce02f957
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-09-24 22:50:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -236,7 +236,7 @@ app.get('/articles/:articleName', function (req, res) {
             res.status(404).send('Article Not found');
         }else {
             var articleData = result.rows[0];
-            res.send(crateTemplate(articleData));
+            res.send(createTemplate(articleData));
         }
     }
     });
