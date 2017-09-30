9dc802c28a806e6d6b80dab32de0d6cc736242b7
chellaperumal <30649506+chellaperumal@users.noreply.github.com>
2017-09-30 07:29:33 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -8,7 +8,7 @@ function loadCommentForm () {
         <input type="submit" id="submit" value="Submit" />
         <br/>
         `;
-    document.getElementById('coment_form').innerHTML = commentFormHtml;
+    document.getElementById('comment_form').innerHTML = commentFormHtml;
     
     // Submit username/password to login
     var submit = document.getElementById('submit');
@@ -41,58 +41,59 @@ function loadCommentForm () {
     };
 }
 
-
-function loadLogin(){
-   var request = new XMLHttpRequest();
-    request.onreadystatechange === function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-             if(request.status === 200){
-                 loadCommentForm(this.responseText);
-                 
+function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadCommentForm(this.responseText);
+            }
         }
-    }
-};
-request.open('GET', '/check-login',true);
-request.send(null);
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
 }
 
-function escapeHTML (text){
-    
+function escapeHTML (text)
+{
     var $text = document.createTextNode(text);
     var $div = document.createElement('div');
     $div.appendChild($text);
     return $div.innerHTML;
-    
 }
 
 function loadComments () {
-     var request = new XMLHttpRequest();
-        request.onreadystatechange === function () {
-        if(request.readyState === XMLHttpRequest.DONE) {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
             var comments = document.getElementById('comments');
-            if(request.status === 200){
+            if (request.status === 200) {
                 var content = '';
                 var commentsData = JSON.parse(this.responseText);
-                for (var i=0; i< commentsData.length; i++){
+                for (var i=0; i< commentsData.length; i++) {
                     var time = new Date(commentsData[i].timestamp);
-                    content += `<div class = "comment">
-                    <p>${escapeHTML(commentsData[i].comment)}</p>
-                    <div class = "commenter">
-                    ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()}
-                    </div>
+                    content += `<div class="comment">
+                        <p>${escapeHTML(commentsData[i].comment)}</p>
+                        <div class="commenter">
+                            ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
+                        </div>
                     </div>`;
                 }
-            comments.innerHTML = content;
-}   else {
-    comments.innerHTML('Oops! could not load comments');
-    
+                comments.innerHTML = content;
+            } else {
+                comments.innerHTML('Oops! Could not load comments!');
+            }
         }
-    }
-};
-request.open('GET', '/get-comments/' + currentArticleTitle,true);
-request.send(null);
+    };
+    
+    request.open('GET', '/get-comments/' + currentArticleTitle, true);
+    request.send(null);
 }
 
 
+// The first thing to do is to check if the user is logged in!
 loadLogin();
 loadComments();
